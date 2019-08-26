9d53e5f4d81585f71d1760692c764dfb1bf2bf25
Jöran Stark <joran.stark@uadm.uu.se>
2019-08-26 21:24:32 +0200

    Updates

---
+++ precache-manifest.6597abfdde4ed55d4359fe7f082844a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21509e4ca404965eadc97882daee243",
+    "url": "/suduko.js/index.html"
+  },
+  {
+    "revision": "4d3f2bc29893dabe4d0a",
+    "url": "/suduko.js/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3a9ebe55c565da6d5097",
+    "url": "/suduko.js/static/js/2.39d7bcae.chunk.js"
+  },
+  {
+    "revision": "4d3f2bc29893dabe4d0a",
+    "url": "/suduko.js/static/js/main.2a4bec53.chunk.js"
+  },
+  {
+    "revision": "35038c65effbe1bb1896",
+    "url": "/suduko.js/static/js/runtime~main.59041670.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/suduko.js/static/media/logo.5d5d9eef.svg"
+  }
+]);
